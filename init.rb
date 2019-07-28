94971489d11895a5dc25b5e7c01e41e1b9a16b54
cryptogopher <git6453hub@michalczyk.pro>
2019-07-28 22:42:50 +0200

    Adding autogenerated plugin files
    command: rails generate redmine_plugin body_tracking

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :body_tracking do
+  name 'Body Tracking plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
