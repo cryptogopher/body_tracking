19e3d54297b2c3edd6476df8844be0554e11634c
cryptogopher <git6453hub@michalczyk.pro>
2019-07-28 23:15:55 +0200

    Added basic plugin description

--- init.rb
+++ init.rb
@@ -1,8 +1,8 @@
 Redmine::Plugin.register :body_tracking do
-  name 'Body Tracking plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
-  version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  name 'Body tracking plugin'
+  author 'cryptogopher'
+  description 'Keep track of body related data to achieve your goals'
+  version '0.1'
+  url 'https://github.com/cryptogopher/body_tracking'
+  author_url 'https://github.com/cryptogopher'
 end
