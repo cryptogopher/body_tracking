f9c5e6d40da9a27a1788076b4b2dccb7a9672a67
cryptogopher <git6453hub@michalczyk.pro>
2019-08-22 23:47:43 +0200

    Unit.name not required

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -2,6 +2,5 @@ class Unit < ActiveRecord::Base
   belongs_to :project
 
   validates :project, associated: true
-  validates :name, presence: true
   validates :shortname, presence: true, uniqueness: {scope: :project_id}
 end
