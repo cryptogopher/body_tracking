b0a02b7474b20088034fadb0ef7d0fe89430357e
cryptogopher <git6453hub@michalczyk.pro>
2019-08-22 17:57:31 +0200

    Added uniqueness constraint to Unit names

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -2,5 +2,5 @@ class Unit < ActiveRecord::Base
   belongs_to :project
 
   validates :project, associated: true
-  validates :name, :shortname, presence: true
+  validates :name, :shortname, presence: true, uniqueness: true
 end
