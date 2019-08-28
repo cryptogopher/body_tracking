b63040b0371e72751850e988cf91cbb96cf084d1
cryptogopher <git6453hub@michalczyk.pro>
2019-08-28 21:04:46 +0200

    Added Ingredients model
    command: rails generate redmine_plugin_model body_tracking ingredient
    name:string ref_unit:references ref_amount:decimal hidden:boolean
    source:references group:integer

--- db/migrate/001_create_units.rb
+++ db/migrate/001_create_units.rb
@@ -1,17 +1,30 @@
 class CreateUnits < ActiveRecord::Migration
   def change
     create_table :units do |t|
-      t.references :project
+      t.references :project, index: true, foreign_key: true
       t.string :name
       t.string :shortname
     end
+    add_index :units, :project_id
 
     create_table :quantities do |t|
-      t.references :project
+      t.references :project, index: true, foreign_key: true
       t.string :name
       t.string :description
       t.integer :domain
     end
+    add_index :quantities, :project_id
+
+    create_table :ingredients do |t|
+      t.string :name
+      t.references :ref_unit, index: true, foreign_key: true
+      t.decimal :ref_amount
+      t.boolean :hidden
+      t.references :source, index: true, foreign_key: true
+      t.integer :group
+    end
+    add_index :ingredients, :ref_unit_id
+    add_index :ingredients, :source_id
 
     reversible do |dir|
       dir.up do
