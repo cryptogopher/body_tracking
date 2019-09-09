9a7a840f3e7928b73c8c06693305664f060adb4d
cryptogopher <git6453hub@michalczyk.pro>
2019-09-09 23:30:02 +0200

    Added further default quantities

--- db/migrate/001_create_units.rb
+++ db/migrate/001_create_units.rb
@@ -60,12 +60,85 @@ class CreateUnits < ActiveRecord::Migration
         f9 = Quantity.create project: nil, domain: :diet, parent: f7, name: "ALA 18:3(n-3)",
           description: "alpha-Linolenic acid"
         f10 = Quantity.create project: nil, domain: :diet, parent: f7, name: "EPA 20:5(n-3)",
-          description: "eicosapentaenoic acid; icosapentaenoic acid"
+          description: "Eicosapentaenoic acid; Icosapentaenoic acid"
         f11 = Quantity.create project: nil, domain: :diet, parent: f7, name: "DHA 22:6(n-3)",
           description: "Docosahexaenoic acid"
 
-        Quantity.create project: nil, domain: :diet, parent: nil, name: "Carbohydrates",
+        c1 = Quantity.create project: nil, domain: :diet, parent: nil, name: "Carbohydrates",
           description: "Total amount of carbohydrates"
+        c2 = Quantity.create project: nil, domain: :diet, parent: c1, name: "Digestible",
+          description: ""
+        c3 = Quantity.create project: nil, domain: :diet, parent: c2, name: "Sugars",
+          description: "Monosaccharides and disaccharides, excluding polyols"
+        c4 = Quantity.create project: nil, domain: :diet, parent: c3, name: "Monosaccharides",
+          description: ""
+        c5 = Quantity.create project: nil, domain: :diet, parent: c4, name: "Glucose",
+          description: ""
+        c6 = Quantity.create project: nil, domain: :diet, parent: c4, name: "Fructose",
+          description: ""
+        c7 = Quantity.create project: nil, domain: :diet, parent: c3, name: "Disaccharides",
+          description: ""
+        c8 = Quantity.create project: nil, domain: :diet, parent: c7, name: "Sucrose",
+          description: ""
+        c9 = Quantity.create project: nil, domain: :diet, parent: c7, name: "Lactose",
+          description: ""
+        c10 = Quantity.create project: nil, domain: :diet, parent: c2, name: "Polyols",
+          description: "Alcohols containing more than 2 hydroxyl groups"
+        c11 = Quantity.create project: nil, domain: :diet, parent: c2,
+          name: "Polysaccharides", description: ""
+        c12 = Quantity.create project: nil, domain: :diet, parent: c11, name: "Starch",
+          description: ""
+        c13 = Quantity.create project: nil, domain: :diet, parent: c1, name: "Indigestible",
+          description: ""
+        c14 = Quantity.create project: nil, domain: :diet, parent: c13, name: "Fibre",
+          description: "Carbohydrate polymers with 3 or more monomeric units, which are" \
+          "neither digested nor absorbed in the human small intestine"
+
+        m1 = Quantity.create project: nil, domain: :diet, parent: nil, name: "Minerals",
+          description: ""
+        m2 = Quantity.create project: nil, domain: :diet, parent: m1, name: "Salt",
+          description: "Sodium chloride"
+
+        v1 = Quantity.create project: nil, domain: :diet, parent: nil, name: "Vitamins",
+          description: ""
+        v2 = Quantity.create project: nil, domain: :diet, parent: v1, name: "Vitamin A",
+          description: ""
+        v3 = Quantity.create project: nil, domain: :diet, parent: v2, name: "Retinol (A1)",
+          description: ""
+        v4 = Quantity.create project: nil, domain: :diet, parent: v1, name: "Provitamin A",
+          description: ""
+        v5 = Quantity.create project: nil, domain: :diet, parent: v4, name: "beta-Carotene",
+          description: ""
+        v6 = Quantity.create project: nil, domain: :diet, parent: v1, name: "Vitamin B",
+          description: ""
+        v7 = Quantity.create project: nil, domain: :diet, parent: v6, name: "Thiamine (B1)",
+          description: ""
+        v8 = Quantity.create project: nil, domain: :diet, parent: v6, name: "Riboflavin (B2)",
+          description: "Other names: Vitamin G"
+        v9 = Quantity.create project: nil, domain: :diet, parent: v6, name: "Vitamin B3",
+          description: "Other names: Vitamin PP"
+        v10 = Quantity.create project: nil, domain: :diet, parent: v9, name: "Niacin",
+          description: "Nicotinic acid"
+        v11 = Quantity.create project: nil, domain: :diet, parent: v6, name: "Vitamin B5",
+          description: "Pantothenic acid"
+        v12 = Quantity.create project: nil, domain: :diet, parent: v6, name: "Vitamin B6",
+          description: ""
+        v13 = Quantity.create project: nil, domain: :diet, parent: v6, name: "Biotin (B7)",
+          description: "Other names: Vitamin H, Coenzyme R"
+        v14 = Quantity.create project: nil, domain: :diet, parent: v6, name: "Folate",
+          description: "Includes: folic acid, folacin and vitamin B9"
+        v15 = Quantity.create project: nil, domain: :diet, parent: v14, name: "Vitamin B9",
+          description: ""
+        v16 = Quantity.create project: nil, domain: :diet, parent: v6,
+          name: "Cobalamin (B12)", description: ""
+        v17 = Quantity.create project: nil, domain: :diet, parent: v1, name: "Vitamin C",
+          description: ""
+        v18 = Quantity.create project: nil, domain: :diet, parent: v1, name: "Vitamin D",
+          description: ""
+        v19 = Quantity.create project: nil, domain: :diet, parent: v1, name: "Vitamin E",
+          description: ""
+        v20 = Quantity.create project: nil, domain: :diet, parent: v1, name: "Vitamin K",
+          description: ""
       end
       dir.down do
         Unit.where(project: nil).delete_all
