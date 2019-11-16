a619b74dcef574c5acdc39ce62406318f15341ad
cryptogopher <git6453hub@michalczyk.pro>
2019-11-16 18:47:53 +0100

    Measurement index/form running

--- app/helpers/measurements_helper.rb
+++ app/helpers/measurements_helper.rb
@@ -1,2 +1,19 @@
 module MeasurementsHelper
+  def quantity_options
+    nested_set_options(@project.quantities.measurement) do |q|
+      raw("#{'&ensp;' * q.level}#{q.name}")
+    end
+  end
+
+  def unit_options
+    @project.units.map do |u|
+      [u.shortname, u.id]
+    end
+  end
+
+  def source_options
+    @project.sources.map do |s|
+      [s.name, s.id]
+    end
+  end
 end
