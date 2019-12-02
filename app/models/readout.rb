8d368d6aa541c1dbae0a4f9a816a10184cf67ef7
cryptogopher <git6453hub@michalczyk.pro>
2019-12-02 23:36:33 +0100

    Fixed updating multiple and deleting Readouts

--- app/models/readout.rb
+++ app/models/readout.rb
@@ -3,6 +3,13 @@ class Readout < ActiveRecord::Base
   belongs_to :quantity, required: true
   belongs_to :unit, required: true
 
-  validates :quantity, uniqueness: {scope: [:measurement_id, :unit_id]}
+  # Uniqueness is checked exclusively on Measurement level. Otherwise validation
+  # may not pass when multiple Readouts are updated at once and some quantity_id
+  # is moved from one Readout to the other (without duplication).
+  # For the same reason Readout quantity_id uniqueness has to be checked by
+  # Measurement when multiple Readouts are first created. Relying on this check
+  # only would make all newly added records pass as valid despite duplications.
+  #validates :quantity, uniqueness: {scope: [:measurement_id, :unit_id]}
+
   validates :value, numericality: true
 end
