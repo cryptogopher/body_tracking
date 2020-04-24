cdd710756788a87e1db6b6cf27bf877ce88fadc3
cryptogopher <git6453hub@michalczyk.pro>
2020-04-24 15:36:16 +0200

    Rearranged Meals index columns

--- app/helpers/meals_helper.rb
+++ app/helpers/meals_helper.rb
@@ -5,9 +5,14 @@ module MealsHelper
     delete_link(meal_path(m), {remote: true, data: {}})
   end
 
-  def display_notes(m)
-    content_tag(:span, m.notes) +
-    link_to(l(:button_notes), edit_notes_meal_path(m),
-            {remote: true, class: "icon icon-wiki-page", style: "float: right"})
+  def adjustment_buttons(i)
+    {'- -' => -10, '-' => -1, '+' => 1, '++' => 10}.map do |text, value|
+      link_to text, adjust_ingredient_path(i, adjustment: value),
+        {remote: true, method: :post, class: "button #{value>0 ? 'green' : 'red'}"}
+    end.reduce(&:+)
+  end
+
+  def notes(m)
+    content_tag(:span, m.notes)
   end
 end
