5e3322fe96fee14bd14c80fd7e28fd2d4978c69c
cryptogopher <git6453hub@michalczyk.pro>
2020-04-25 01:08:33 +0200

    Removed quantity uniqueness validation

--- app/models/nutrient.rb
+++ app/models/nutrient.rb
@@ -3,6 +3,8 @@ class Nutrient < ActiveRecord::Base
   belongs_to :quantity, required: true
   belongs_to :unit, required: true
 
-  validates :quantity, uniqueness: {scope: :food_id}
+  # Uniqueness is checked exclusively on Food level (see Readout model for details)
+  #validates :quantity, uniqueness: {scope: :food_id}
+
   validates :amount, numericality: {greater_than_or_equal_to: 0.0}
 end
