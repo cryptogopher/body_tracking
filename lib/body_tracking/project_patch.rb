c029ba59c226a622e5f2118cf197526fcc65b17f
cryptogopher <git6453hub@michalczyk.pro>
2020-04-25 18:07:58 +0200

    Fixed saving Meal exposures
    According to:
    https://stackoverflow.com/questions/34328735/imageable-type-is-not-setting-for-polymorphic-associations
    and
    https://stackoverflow.com/questions/2494452/rails-polymorphic-association-with-multiple-associations-on-the-same-model

--- lib/body_tracking/project_patch.rb
+++ lib/body_tracking/project_patch.rb
@@ -6,7 +6,8 @@ module BodyTracking::ProjectPatch
 
     has_many :foods, -> { order "name" }, dependent: :destroy,
       extend: BodyTracking::ItemsWithQuantities
-    has_many :nutrient_exposures, as: :view, dependent: :destroy,
+    has_many :nutrient_exposures, -> { where view_type: "Nutrient" }, dependent: :destroy,
+      foreign_key: :view_id, foreign_type: :view_type,
       class_name: 'Exposure', extend: BodyTracking::TogglableExposures
     has_many :nutrient_quantities, -> { order "lft" }, through: :nutrient_exposures,
       source: 'quantity'
@@ -16,7 +17,8 @@ module BodyTracking::ProjectPatch
       extend: BodyTracking::ItemsWithQuantities, through: :measurement_routines
 
     has_many :meals, -> { order "eaten_at DESC" }, dependent: :destroy
-    has_many :meal_exposures, as: :view, dependent: :destroy,
+    has_many :meal_exposures, -> { where view_type: "Meal" }, dependent: :destroy,
+      foreign_key: :view_id, foreign_type: :view_type,
       class_name: 'Exposure', extend: BodyTracking::TogglableExposures
     has_many :meal_quantities, -> { order "lft" }, through: :meal_exposures,
       source: 'quantity'
