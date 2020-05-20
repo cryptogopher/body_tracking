976271d6b461475c413065de535e69b3d7eb85fa
cryptogopher <git6453hub@michalczyk.pro>
2020-05-21 00:23:38 +0200

    Fixed association

--- app/models/readout.rb
+++ app/models/readout.rb
@@ -1,6 +1,6 @@
 class Readout < QuantityValue
-  belongs_to :measurement, foreign_key: 'registry_id', inverse_of: :readouts,
-    polymorphic: true, required: true
+  belongs_to :measurement, foreign_key: 'registry_id', foreign_type: 'registry_type',
+    inverse_of: :readouts, polymorphic: true, required: true
 
   # Uniqueness NOT validated here, see Value for explanation
   #validates :quantity, uniqueness: {scope: [:measurement_id, :unit_id]}
